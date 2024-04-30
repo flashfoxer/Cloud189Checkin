05e069c503e61d752c59c131b5710e95693f83c1
flashfoxer <122686750+flashfoxer@users.noreply.github.com>
2024-04-30 10:46:33 +0800

    Update wxPusher.js

--- src/push/wxPusher.js
+++ src/push/wxPusher.js
@@ -1,5 +1,5 @@
 module.exports = {
   appToken:
-    process.env.WX_PUSHER_APP_TOKEN || "AT_OzwCik0QP4p4AQlKdO4jbgjWApTiWsJr",
-  uid: process.env.WX_PUSHER_UID || "",
+    process.env.WX_PUSHER_APP_TOKEN || "AT_Vxq1o4XEcCB6aQHOq8AURxaFiqsKrMis",
+  uid: process.env.WX_PUSHER_UID || "UID_0DhtGJqXfxYDE8GJicytE5EAoNdX",
 };
